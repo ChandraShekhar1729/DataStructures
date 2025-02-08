63719a427e5fd8c521aadc6c6517d06e33235d0a
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2025-02-08 20:26:53 +0530

    Some Basic SetUp

--- DSA.Test/Program.cs
+++ DSA.Test/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("This is the Hello World");
+﻿using DSA.Chandrashekhar;
+using DSA.Shayamli;
+
+namespace DSA.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // These Two instances will be always there as there are used to create the instances of the console Projects
+            Functions chandu = new Functions();
+            Function shayamli = new Function();
+
+            Console.WriteLine(shayamli.getRandomNum());
+        }
+    }
+}
