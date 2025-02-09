a6e0e834a54cb68fcfdcf11c0faa06ad824e12c5
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2025-02-09 23:33:14 +0530

    Finding Min and Max in an Array

--- DSA.Chandrashekhar/Functions.cs
+++ DSA.Chandrashekhar/Functions.cs
@@ -2,6 +2,24 @@
 {
     public class Functions
     {
-          
+        // Method to find the max and Min in an Array
+
+        public int[] FindMinAndMax(int[] arr)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            foreach( var item in arr)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            return new int[] { min, max };
+        }    
     }
 }
